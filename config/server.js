6edddf239334eceb3c85bc439989cdb233d4eedc
Jon Hansen <jon.hansen@mu.st>
2023-12-20 10:42:17 +0100

    Change app keys into env variables

--- config/server.js
+++ config/server.js
@@ -2,9 +2,11 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: ['kissMeyer', 'jungleRas'],
+    keys: env.array('APP_KEYS', ['defaultKeyA', 'defaultKeyB']), // Fallback keys are optional
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
+
+
