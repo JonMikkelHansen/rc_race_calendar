f6067529ef7f96bf87745ea43a76fe150fa1cabd
Jon Hansen <jon.hansen@mu.st>
2024-01-18 22:52:32 +0100

    Adjustable Y-axis v2

--- gpx-editor/src/components/GPXViz.js
+++ gpx-editor/src/components/GPXViz.js
@@ -2,46 +2,46 @@ import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
 import { useSelector, useDispatch } from 'react-redux';
-import { setTolerance, setMinY, setMaxY } from '../redux/actions/GPXActions'; // Make sure setMaxY is imported
+import { setTolerance, setMinY, setMaxY } from '../redux/actions/GPXActions';
 import { douglasPeucker } from '../Utilities';
 
 const GPXViz = ({ gpxData }) => {
   const dispatch = useDispatch();
   const reduxTolerance = useSelector(state => state.tolerance);
   const reduxMinY = useSelector(state => state.minY);
-  const reduxMaxY = useSelector(state => state.maxY); // Y-max state
+  const reduxMaxY = useSelector(state => state.maxY);
   const [simplifiedData, setSimplifiedData] = useState([]);
   const [initialYSet, setInitialYSet] = useState(false);
 
   useEffect(() => {
-    if (!gpxData.tracks?.[0].segments?.[0] || initialYSet) {
-      return;
+    // Apply Douglas-Peucker simplification whenever tolerance changes
+    if (gpxData.tracks?.[0].segments?.[0]) {
+      const originalTrackPoints = gpxData.tracks[0].segments[0];
+      const simplifiedTrackPoints = douglasPeucker(originalTrackPoints, reduxTolerance);
+      setSimplifiedData(simplifiedTrackPoints);
     }
+  }, [gpxData, reduxTolerance]);
 
-    const originalTrackPoints = gpxData.tracks[0].segments[0];
-    const simplifiedTrackPoints = douglasPeucker(originalTrackPoints, reduxTolerance);
-    setSimplifiedData(simplifiedTrackPoints);
+  useEffect(() => {
+    // Set initial minY and maxY only once
+    if (!initialYSet && gpxData.tracks?.[0].segments?.[0]) {
+      const elevations = gpxData.tracks[0].segments[0].map(p => p.elevation);
+      const lowestElevation = Math.floor(Math.min(...elevations) / 100) * 100;
+      const highestElevation = Math.ceil(Math.max(...elevations) / 100) * 100;
+      dispatch(setMinY(lowestElevation));
+      dispatch(setMaxY(highestElevation));
+      setInitialYSet(true);
+    }
+  }, [gpxData, dispatch, initialYSet]);
 
-    // Set initial minY and maxY values
-    const elevations = originalTrackPoints.map(p => p.elevation);
-    const lowestElevation = Math.floor(Math.min(...elevations) / 100) * 100;
-    const highestElevation = Math.ceil(Math.max(...elevations) / 100) * 100;
-    dispatch(setMinY(lowestElevation));
-    dispatch(setMaxY(highestElevation));
-    setInitialYSet(true);
-  }, [gpxData, reduxTolerance, dispatch, initialYSet]);
-
-  // Tolerance change handler
   const handleToleranceChange = (e) => {
     dispatch(setTolerance(parseFloat(e.target.value)));
   };
 
-  // MinY change handler
   const handleMinYChange = (e) => {
     dispatch(setMinY(Number(e.target.value)));
   };
 
-  // MaxY change handler
   const handleMaxYChange = (e) => {
     dispatch(setMaxY(Number(e.target.value)));
   };
@@ -64,7 +64,7 @@ const GPXViz = ({ gpxData }) => {
     scales: {
       y: {
         min: reduxMinY,
-        max: reduxMaxY, // Set the maximum value of the Y-axis
+        max: reduxMaxY,
       },
     },
   };
