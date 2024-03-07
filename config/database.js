8c3c45781de17a8dfbb2d2df8153711b268487dc
Jon Hansen <jon.hansen@mu.st>
2024-03-07 13:50:05 +0100

    Changes to DB setup

--- config/database.js
+++ config/database.js
@@ -1,19 +1,16 @@
-module.exports = ({ env }) => {
-  // Determine environment mode
-  const isProduction = env('NODE_ENV') === 'production';
-
-  // Database configuration
-  const databaseConfig = {
-    client: env('DATABASE_CLIENT', isProduction ? env('PROD_DATABASE_CLIENT') : env('DEV_DATABASE_CLIENT')),
+module.exports = ({ env }) => ({
+  connection: {
+    client: env('DATABASE_CLIENT'),
     connection: {
-      host: env(isProduction ? 'PROD_DATABASE_HOST' : 'DEV_DATABASE_HOST'),
-      port: env.int(isProduction ? 'PROD_DATABASE_PORT' : 'DEV_DATABASE_PORT'),
-      database: env(isProduction ? 'PROD_DATABASE_NAME' : 'DEV_DATABASE_NAME'),
-      user: env(isProduction ? 'PROD_DATABASE_USERNAME' : 'DEV_DATABASE_USERNAME'),
-      password: env(isProduction ? 'PROD_DATABASE_PASSWORD' : 'DEV_DATABASE_PASSWORD'),
-      ssl: isProduction ? { rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true) } : false,
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL') ? { rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false) } : false,
     },
-  };
-
-  return { connection: databaseConfig };
-};
+    // Here you can include other configuration settings like pool configuration, etc.
+    // ...
+  },
+  // ... any other configuration outside of the connection object
+});
