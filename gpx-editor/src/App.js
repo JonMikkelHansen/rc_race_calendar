3546686efee441cc9baecb1a700f3a5d72255496
Jon Hansen <jon.hansen@mu.st>
2024-03-15 11:52:31 +0100

    Tabs added

--- gpx-editor/src/App.js
+++ gpx-editor/src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/common/Header';
+import Tabs from './components/common/Tabs'; // Note: We'll modify Tabs.js to include the tabs functionality
 import RaceList from './components/RaceList';
 import GPXUploader from './components/GPX/GPXUploader';
 import GPXViz from './components/GPX/GPXViz';
 import WaypointEditor from './components/GPX/WaypointEditor';
 import SegmentEditor from './components/GPX/SegmentEditor';
-import GPXSave from './components/GPXSave'; // Import GPXSave component
+import GPXSave from './components/GPXSave';
 
 function App() {
   const [gpxData, setGpxData] = useState(null);
+  const [activeTab, setActiveTab] = useState('GPX'); // Added state to manage active tab
 
   const handleGPXData = (data) => {
     setGpxData(data);
@@ -19,19 +21,23 @@ function App() {
     <Router>
       <div className="App">
         <Header />
-        {/*<RaceList /> */}
-        <GPXUploader onGPXData={handleGPXData} />
-        {gpxData && (
+        <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        {activeTab === 'Race' && <RaceList />}
+        {activeTab === 'GPX' && (
           <>
-            <GPXViz gpxData={gpxData} />
-            <WaypointEditor gpxData={gpxData} onEdit={handleGPXData} />
-            <SegmentEditor gpxData={gpxData} onEdit={handleGPXData} />
-            <GPXSave /> {/* Add Save GPX component here */}
+            <GPXUploader onGPXData={handleGPXData} />
+            {gpxData && (
+              <>
+                <GPXViz gpxData={gpxData} />
+                <WaypointEditor gpxData={gpxData} onEdit={handleGPXData} />
+                <SegmentEditor gpxData={gpxData} onEdit={handleGPXData} />
+                <GPXSave /> {/* GPX Save component */}
+              </>
+            )}
           </>
         )}
       </div>
     </Router>
-
   );
 }
 
---
+++ gpx-editor/src/components/common/Tabs.js
@@ -0,0 +1,45 @@
+// Assuming Tabs.js is located under ./components/common/Tabs
+import React from 'react';
+import styled from 'styled-components';
+
+const TabsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1em;
+`;
+
+const Tab = styled.button`
+  padding: 10px;
+  margin-right: 5px;
+  border: none;
+  background-color: ${props => props.isActive ? '#fff' : '#ccc'};
+  color: ${props => props.isActive ? '#000' : '#666'};
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+const Tabs = ({ activeTab, setActiveTab }) => {
+  return (
+    <TabsContainer>
+      <Tab isActive={activeTab === 'Race'} onClick={() => setActiveTab('Race')}>Race</Tab>
+      <Tab isActive={activeTab === 'GPX'} onClick={() => setActiveTab('GPX')}>GPX</Tab>
+      <Tab isActive={activeTab === 'Meta'} onClick={() => setActiveTab('Meta')}>Meta</Tab>
+      <Tab isActive={activeTab === 'Other'} onClick={() => setActiveTab('Other')}>Other</Tab>
+      <Tab isActive={activeTab === 'Save'} onClick={() => setActiveTab('Save')}>Save</Tab>
+    </TabsContainer>
+  );
+};
+
+export default Tabs;
+
+
+
+
+
