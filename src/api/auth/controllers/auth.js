9b1092939801b691c0c1af556689795a5a384c10
Jon Hansen <jon.hansen@mu.st>
2024-04-16 16:55:56 +0200

    Update 3

--- src/api/auth/controllers/auth.js
+++ src/api/auth/controllers/auth.js
@@ -1,5 +1,3 @@
-// path: src/api/auth/controllers/auth.js
-
 const { google } = require('googleapis');
 
 module.exports = {
@@ -7,17 +5,37 @@ module.exports = {
     const oauth2Client = new google.auth.OAuth2(
       process.env.GOOGLE_CLIENT_ID,
       process.env.GOOGLE_CLIENT_SECRET,
-      process.env.GOOGLE_REDIRECT_URI
+      `${process.env.BACKEND_URL}/auth/google/callback`
     );
+
     const url = oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+      access_type: 'offline',  // Ensure that you receive a refresh token along with the access token
+      scope: [
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/userinfo.profile',
+      ]
     });
+
     ctx.redirect(url);
   },
 
   async handleGoogleCallback(ctx) {
+    const oauth2Client = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      `${process.env.BACKEND_URL}/auth/google/callback`
+    );
+
     const { code } = ctx.query;
-    // Exchange code for tokens, save them and handle authentication session
+    try {
+      const { tokens } = await oauth2Client.getToken(code);
+      oauth2Client.setCredentials(tokens);
+      // Handle your login/session creation here
+      ctx.body = { message: 'Authentication successful', tokens };
+    } catch (error) {
+      console.error('Error during the Google authentication', error);
+      ctx.body = { message: 'Authentication failed', error };
+      ctx.status = 500;
+    }
   }
 };
--- src/api/auth/routes/auth.js
+++ src/api/auth/routes/auth.js
@@ -1,17 +1,24 @@
 module.exports = {
-    routes: [
-      {
-        method: 'GET',
-        path: '/api/auth/google',
-        handler: 'auth.redirectToGoogle',
-        config: { auth: false },
-      },
-      {
-        method: 'GET',
-        path: '/api/auth/google/callback',
-        handler: 'auth.handleGoogleCallback',
-        config: { auth: false },
+  routes: [
+    {
+      method: 'GET',
+      path: '/auth/google',
+      handler: 'auth.redirectToGoogle',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
       }
-    ]
-  };
-  
+    },
+    {
+      method: 'GET',
+      path: '/auth/google/callback',
+      handler: 'auth.handleGoogleCallback',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      }
+    }
+  ]
+};
