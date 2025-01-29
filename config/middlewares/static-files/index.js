49af4f5e2d7dee467f0be3319741ee438d2c1c19
JonMikkelHansen <jon.mikkel.hansen@gmail.com>
2025-01-29 09:58:44 +0100

    Attempt to fix middleware

--- config/middlewares/static-files/index.js
+++ config/middlewares/static-files/index.js
@@ -1,9 +1,18 @@
 const serve = require('koa-static');
 const path = require('path');
 
-module.exports = (config, { strapi }) => async (ctx, next) => {
-  if (ctx.path.startsWith(config.publicPath)) {
-    return serve(path.resolve(strapi.dirs.public, config.directory))(ctx, next);
-  }
-  await next();
+module.exports = {
+  name: 'static-files',
+  factory: (config, { strapi }) => {
+    return {
+      initialize() {
+        return async (ctx, next) => {
+          if (ctx.path.startsWith(config.publicPath)) {
+            return serve(path.resolve(strapi.dirs.public, config.directory))(ctx, next);
+          }
+          await next();
+        };
+      },
+    };
+  },
 };
