05c26fad420008bfce20e29e29500225c63584c1
JonMikkelHansen <jon.mikkel.hansen@gmail.com>
2025-02-12 21:05:33 +0100

    Fixing middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,7 +8,14 @@ module.exports = [
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public',
+  {
+    name: 'strapi::public',
+    config: {
+      enabled: true,
+      path: './public',  // serves files from the public directory
+      defaultIndex: false,
+    }
+  },
   {
     name: 'custom::static-files',
     config: {
